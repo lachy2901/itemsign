bc000d39da2278d7dc9d7f4110b631e45d05ef01
unknown <LHolden@ACER32101651601.ic.local>
2013-01-15 10:27:29 +1030

    Fix wall signs and add damage values

--- com/edgegames/lachy2901/itemsign/ItemSign.java
+++ com/edgegames/lachy2901/itemsign/ItemSign.java
@@ -5,7 +5,6 @@ import org.bukkit.Material;
 import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -18,14 +17,15 @@ public class ItemSign extends JavaPlugin implements Listener {
 	
 	@EventHandler
 	public void onPlayerInteract(PlayerInteractEvent event) {
-		if (Material.SIGN.equals(event.getClickedBlock().getType()) || Material.SIGN_POST.equals(event.getClickedBlock().getType())) {
+		if (Material.WALL_SIGN.equals(event.getClickedBlock().getType()) || Material.SIGN_POST.equals(event.getClickedBlock().getType())) {
 			Sign sign = (Sign) event.getClickedBlock().getState();
 			if (sign.getLine(1).equalsIgnoreCase("Item ID:")) {
-				int ID = Integer.parseInt(sign.getLine(3));
-				if (event.getAction() == Action.LEFT_CLICK_BLOCK) {
-					event.getPlayer().getInventory().addItem(new ItemStack(ID, 64));
-				} else if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-					event.getPlayer().getInventory().addItem(new ItemStack(ID, 1));
+				event.setCancelled(true);
+				if (sign.getLine(2).contains(":")) {
+					String[] parts = sign.getLine(2).split(":");
+					event.getPlayer().getInventory().addItem(new ItemStack(Integer.parseInt(parts[0]), 64, Short.valueOf(parts[1])));
+				} else {
+					event.getPlayer().getInventory().addItem(new ItemStack(Integer.parseInt(sign.getLine(2)), 64));
 				}
 			}
 		}
